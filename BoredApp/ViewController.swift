bbc7db00b1947249feaef8996e4fe46d8b9f232a
Afir Thes <afirthes@gmail.com>
2021-09-11 14:29:10 +0300

    Open api interaction and UI tunning

--- BoredApp/ViewController.swift
+++ BoredApp/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wikiLink: UILabel!
+    @IBOutlet weak var activityTextView: UITextView!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var participantsTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var accesibilityTextField: UITextField!
+    
+    @IBOutlet weak var findButton: UIButton!
+    
+    let urlString = "https://www.boredapi.com/api/activity"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        findButton.layer.cornerRadius = findButton.frame.height / 4
+    
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        
+        activityTextView.layer.borderWidth = 0.5
+        activityTextView.layer.cornerRadius = 5
+        activityTextView.layer.borderColor = CGColor(gray: 0.25, alpha: 0.25)
+        
     }
 
+    @IBAction func doFind(_ sender: Any) {
+        AF.request(urlString).responseJSON{ (response) in
+            if let jsonData = response.value as? [String:Any] {
+                
+                self.activityTextView.text = jsonData["activity"] as? String
+                self.typeTextField.text = jsonData["type"] as? String
+                self.participantsTextField.text = "\(String(describing: jsonData["participants"] ?? "--"))"
+                self.priceTextField.text = "\(String(describing: jsonData["price"] ?? "--"))"
+                self.accesibilityTextField.text = "\(String(describing: jsonData["accessibility"] ?? "--"))"
+                
+                if let link = jsonData["link"] as? String {
+                    if(link != "") {
+                        
+                        
+                        let attributedString = NSMutableAttributedString(string: link)
+                        attributedString.addAttribute(.link, value: link, range: NSRange(location: 0, length: link.count ))
 
+                        self.wikiLink.attributedText = attributedString
+                        
+                    } else {
+                        self.wikiLink.attributedText = NSMutableAttributedString(string: "[no link]")
+                    }
+                }
+            }
+        }
+    }
+    
 }
 
